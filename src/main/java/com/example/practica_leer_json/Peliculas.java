fa5326f13291ebb1f1aec7b33ba8bf199e91692c
PaulaNuness <127285238+PaulaNuness@users.noreply.github.com>
2023-10-27 12:14:02 +0200

    Okey_con imagen añadida

---
+++ src/main/java/com/example/practica_leer_json/Peliculas.java
@@ -0,0 +1,52 @@
+package com.example.practica_leer_json;
+
+public class Peliculas {
+    private int id;
+    private String titulo;
+    private String fecha;
+    private String director;
+    private String genero;
+
+    public Peliculas() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
